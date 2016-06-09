732a339767043d084509e6e598ddca82ca1456ba
Artem Shuba <ashuba@yamoney.ru>
2016-06-09 15:37:50 +0300

    DistinctBy

--- Utils/Extensions/CollectionsExtensions.cs
+++ Utils/Extensions/CollectionsExtensions.cs
@@ -239,5 +239,17 @@ namespace Jupiter.Utils.Extensions
                 return max;
             }
         }
+
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            HashSet<TKey> seenKeys = new HashSet<TKey>();
+            foreach (TSource element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }
     }
 }
