31f19a13ebf57ed464042a999e468ab14c722f8b
Artem Shuba <a.shuba@outlook.com>
2016-12-22 23:30:17 +0300

    DelegateCommand

--- Jupiter.UWP/Mvvm/DelegateCommand.cs
+++ Jupiter.UWP/Mvvm/DelegateCommand.cs
@@ -1,82 +1,158 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Windows.Input;
 
 namespace Jupiter.Mvvm
 {
     // DOCS: https://github.com/Windows-XAML/Template10/wiki/Docs-%7C-MVVM
-    public class DelegateCommand : System.Windows.Input.ICommand
+
+    public class DelegateCommand : ICommand
+
     {
+
         private readonly Action _execute;
+
         private readonly Func<bool> _canExecute;
+
         public event EventHandler CanExecuteChanged;
+
+
 
         public DelegateCommand(Action execute, Func<bool> canexecute = null)
+
         {
+
             if (execute == null)
+
                 throw new ArgumentNullException(nameof(execute));
+
             _execute = execute;
+
             _canExecute = canexecute ?? (() => true);
+
         }
 
+
+
         [DebuggerStepThrough]
+
         public bool CanExecute(object p = null)
+
         {
+
             try { return _canExecute(); }
+
             catch { return false; }
+
         }
+
+
 
         public void Execute(object p = null)
+
         {
+
             if (!CanExecute(p))
+
                 return;
+
             try { _execute(); }
+
             catch { Debugger.Break(); }
+
         }
+
+
 
         public void RaiseCanExecuteChanged()
+
         {
+
             CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
         }
+
     }
 
-    // DOCS: https://github.com/Windows-XAML/Template10/wiki/Docs-%7C-MVVM
-    public class DelegateCommand<T> : System.Windows.Input.ICommand
+
+
+    // DOCS: https://github.com/Windows-XAML/Template10/wiki/MVVM
+
+    public class DelegateCommand<T> : ICommand
+
     {
+
         private readonly Action<T> _execute;
+
         private readonly Func<T, bool> _canExecute;
+
         public event EventHandler CanExecuteChanged;
 
+
+
         public DelegateCommand(Action<T> execute, Func<T, bool> canexecute = null)
+
         {
+
             if (execute == null)
+
                 throw new ArgumentNullException(nameof(execute));
+
             _execute = execute;
+
             _canExecute = canexecute ?? (e => true);
+
         }
+
+
 
         [DebuggerStepThrough]
+
         public bool CanExecute(object p)
+
         {
-            try
-            {
-                var _Value = (T)Convert.ChangeType(p, typeof(T));
-                return _canExecute == null ? true : _canExecute(_Value);
-            }
+
+            try { return _canExecute(ConvertParameterValue(p)); }
+
             catch { return false; }
+
         }
+
+
 
         public void Execute(object p)
+
         {
-            if (!CanExecute(p))
+
+            if (!this.CanExecute(p))
+
                 return;
-            var _Value = (T)Convert.ChangeType(p, typeof(T));
-            _execute(_Value);
+
+            _execute(ConvertParameterValue(p));
+
         }
+
+
+
+        private static T ConvertParameterValue(object parameter)
+
+        {
+
+            parameter = parameter is T ? parameter : Convert.ChangeType(parameter, typeof(T));
+
+            return (T)parameter;
+
+        }
+
+
 
         public void RaiseCanExecuteChanged()
-        {
-            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
-        }
-    }
 
+        {
+
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+        }
+
+    }
 }
