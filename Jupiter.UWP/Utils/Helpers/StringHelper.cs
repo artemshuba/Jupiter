46f9f97c6dd6ac4a4a426db6c478583c1774a54d
Artem Shuba <a.shuba@outlook.com>
2017-06-22 11:56:29 +0300

    StringHelper fix

--- Jupiter.UWP/Utils/Helpers/StringHelper.cs
+++ Jupiter.UWP/Utils/Helpers/StringHelper.cs
@@ -5,9 +5,12 @@
         /// <summary>
         /// Numerals localization
         /// </summary>
-        public static string LocalizeNumerals(int value, string singularString, string dualString, string pluralString)
+        public static string LocalizeNumerals(int value, string singularString, string dualString, string pluralString, string locale)
         {
             string result = string.Empty;
+
+            if (!string.IsNullOrEmpty(locale) && locale.StartsWith("en"))
+                return value == 1 ? singularString : pluralString;
 
             int plural = (value % 10 == 1 && value % 100 != 11 && value != 0 ? 0 : value % 10 >= 2 && value % 10 <= 4 && (value % 100 < 10 || value % 100 >= 20) && value != 0 ? 1 : 2);
             switch (plural)
